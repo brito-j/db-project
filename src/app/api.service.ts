3f3877a637ec96be92bf385e92374f401a975ca3
jason <jasonbrito98@gmail.com>
2020-03-31 16:17:58 -0400

    Configure module api and service api for HTTP

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  api = "http://127.0.0.1:80";
+
+  constructor(private httpClient: HttpClient) { }
+
+  test(): any {
+    return this.httpClient.get("");
+  }
 }
