fb10eded9b1c203e41fee08138f55336c80c6dd8
jason <jasonbrito98@gmail.com>
2020-04-05 12:09:42 -0400

    Implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {ReadComponent} from './read/read.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'read',
+    component: ReadComponent
+  },
+  { path: '',
+    redirectTo: '/about',
+    pathMatch: 'full'
+  },
+  { path: '**',
+    redirectTo: '/about'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
