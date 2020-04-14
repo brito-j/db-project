67772fb5babcd5777bb5f5d30c814f2d3d344c01
jason <jasonbrito98@gmail.com>
2020-04-13 23:27:49 -0400

    Implement CRUD for worksite-info

--- src/app/worksite-info/worksite-info.component.ts
+++ src/app/worksite-info/worksite-info.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Application} from '../interfaces/application';
 import {ApiService} from '../api.service';
 import {WorksiteInfo} from '../interfaces/worksite-info';
 
@@ -11,13 +10,77 @@ import {WorksiteInfo} from '../interfaces/worksite-info';
 export class WorksiteInfoComponent implements OnInit {
 
   data: WorksiteInfo[] = [];
-  selected: WorksiteInfo[] = [];
+  openUpdate: boolean = false;
+  openCreate: boolean = false;
+
+  case_number: string = "";
+  public_disclosure_location: string = "";
+  worksite_city: string = "";
+  worksite_county: string = "";
+  worksite_state: string = "";
+  worksite_postal_code: string = "";
+
+  createData: FormData = new FormData();
+  updateData: FormData = new FormData();
+  deleteData: FormData = new FormData();
+  table: string = "worksite_info";
 
   constructor(private apiService: ApiService) { }
+
+  onCreate(): void {
+    this.clear();
+    this.openCreate = true;
+  }
+
+  create(): void {
+    this.createData.append('case_number', this.case_number);
+    this.createData.append('public_disclosure_location', this.public_disclosure_location);
+    this.createData.append('worksite_city', this.worksite_city);
+    this.createData.append('worksite_county', this.worksite_county);
+    this.createData.append('worksite_state', this.worksite_state);
+    this.createData.append('worksite_postal_code', this.worksite_postal_code);
+    this.apiService.create(this.createData, this.table).subscribe(() => {
+      location.reload()
+    });
+  }
 
   read(): void {
     this.apiService.read("worksite_info")
       .subscribe((data: WorksiteInfo[]) => { this.data = data })
+  }
+
+  onUpdate(datum): void {
+    this.clear();
+    this.openUpdate = true;
+    this.case_number = datum.case_number;
+  }
+
+  update(): void {
+    this.updateData.append('case_number', this.case_number);
+    this.updateData.append('public_disclosure_location', this.public_disclosure_location);
+    this.updateData.append('worksite_city', this.worksite_city);
+    this.updateData.append('worksite_county', this.worksite_county);
+    this.updateData.append('worksite_state', this.worksite_state);
+    this.updateData.append('worksite_postal_code', this.worksite_postal_code);
+    this.apiService.update(this.updateData, this.table).subscribe(() => {
+      location.reload()
+    });
+  }
+
+  delete(datum): void {
+    this.case_number = datum.case_number;
+    this.deleteData.append('table', this.table);
+    this.deleteData.append('case_number', this.case_number);
+    this.apiService.delete(this.deleteData).subscribe(() => {location.reload()})
+  }
+
+  clear(): void {
+    this.case_number = "";
+    this.public_disclosure_location = "";
+    this.worksite_city = "";
+    this.worksite_county = "";
+    this.worksite_state = "";
+    this.worksite_postal_code = "";
   }
 
   ngOnInit(): void { this.read() }
